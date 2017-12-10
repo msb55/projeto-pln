c7fe805d46250bd1b62d90fe4040ebf8bf83e3d3
Marcos Barreto <msb5@cin.ufpe.br>
2017-12-10 02:43:17 -0300

    setup inicial com python

---
+++ script.py
@@ -0,0 +1,40 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+import six
+
+def syntax_text(text):
+    """Detects syntax in the text."""
+    client = language.LanguageServiceClient()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode('utf-8')
+
+    # Instantiates a plain text document.
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT,
+        language='pt')
+
+    # Detects syntax in the document. You can also analyze HTML with:
+    #   document.type == enums.Document.Type.HTML
+    tokens = client.analyze_syntax(document).tokens
+
+    # part-of-speech tags from enums.PartOfSpeech.Tag
+    pos_tag = ('UNKNOWN', 'ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM',
+               'PRON', 'PRT', 'PUNCT', 'VERB', 'X', 'AFFIX')
+
+    # Detects entities in the document. You can also analyze HTML with:
+    #   document.type == enums.Document.Type.HTML
+    entities = client.analyze_entities(document).entities
+
+    # entity types from enums.Entity.Type
+    entity_type = ('UNKNOWN', 'PERSON', 'LOCATION', 'ORGANIZATION',
+                   'EVENT', 'WORK_OF_ART', 'CONSUMER_GOOD', 'OTHER')
+
+    for token in tokens:
+        # print(u'{}: {}'.format(pos_tag[token.part_of_speech.tag], token.text.content))
+        print(u'{}: {}'.format(str(token.part_of_speech), token.text.content))
+
+syntax_text("O secretário é muito inteligente.")
